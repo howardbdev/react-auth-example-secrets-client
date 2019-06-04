30880615d2bf3f785f15e7543850e00e85d3ccc9
howardbdev <howarddevennish@gmail.com>
2019-06-04 15:19:18 -0400

    change App to class, add handlers and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login"
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      currentUser: null,
+      loginForm: {
+        email: "",
+        password: ""
+      }
+    }
+  }
+
+  handleLoginFormChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      loginForm: {
+        ...this.state.loginForm,
+        [name]: value
+      }
+    })
+  }
+
+  handleLoginFormSubmit = event => {
+    event.preventDefault()
+    // now I need to submit the info from the form to the back end
+    // ... where I will authenticate the user, and if valid, send the user back
+    // with that response, set my state, all is glorious with the world
+    const userInfo = this.state.loginForm
+    const headers = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: userInfo
+      })
+    }
+    fetch("http://localhost:3001/login", headers)
+      .then(r => r.json())
+      .then(userJSON => {
+        if (userJSON.error) {
+          // failure
+          alert("invalid credentials")
+        } else {
+          // success
+          this.setState({
+            currentUser: userJSON.user
+          })
+
+        }
+      })
+      .catch(console.log)
+  }
+
+  render() {
+    const { currentUser } = this.state
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{ currentUser ?
+        `Logged in as ${currentUser.name}` :
+        "Not logged in"
+       }</h2>
+
+      <Login
+        handleLoginFormChange={this.handleLoginFormChange}
+        handleLoginFormSubmit={this.handleLoginFormSubmit}
+        email={this.state.loginForm.email}
+        password={this.state.loginForm.password}
+      />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
